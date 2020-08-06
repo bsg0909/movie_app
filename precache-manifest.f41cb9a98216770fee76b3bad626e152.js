3d7e8094d816c2b17488be37172cb2f4303821e5
bsg0909 <bsg0909@daum.net>
2020-08-06 14:45:57 +0900

    Updates

---
+++ precache-manifest.f41cb9a98216770fee76b3bad626e152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7c1ef7499935fd0488138f12d88f81",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "dc276c2a6fc117cb8a04",
+    "url": "/movie_app/static/css/main.bb1aabe5.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc276c2a6fc117cb8a04",
+    "url": "/movie_app/static/js/main.7107de84.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
